53e3d9db01a523595dbcf8394c71e582f2ea8a6e
Daniel Weber <dan.b.weber@me.com>
2018-07-14 15:06:39 -0400

    this gets images displaying on the page in a very basic manner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  urls: string[];
+
+  ngOnInit() {
+    this.urls = [
+      "https://imgs.xkcd.com/comics/soda_sugar_comparisons.png",
+      "https://imgs.xkcd.com/comics/success.png",
+      "https://imgs.xkcd.com/comics/going_west.png",
+      "https://imgs.xkcd.com/comics/bad_map_projection_time_zones.png",
+      "https://imgs.xkcd.com/comics/actors.png",
+      "https://imgs.xkcd.com/comics/unscientific.png",
+      "https://imgs.xkcd.com/comics/literally.png",
+      "https://imgs.xkcd.com/comics/induced_current.png",
+    ]
+  }
 }
