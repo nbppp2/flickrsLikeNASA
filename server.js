f98da69b215c082079be5016d75292c070a68490
Daniel Weber <dan.b.weber@me.com>
2018-07-14 17:20:15 -0400

    quick fixes

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 // server.js
 const express = require('express');
+const path = require('path');
 const app = express();
+
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/flickrsLikeNASA/index.html'));
+});
+
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, function () {
+  console.log('Listening on port ' + this.address().port); //Listening on port 8888
+});
